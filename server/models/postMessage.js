123d11d23479f78ca4ff47feaa358bda826fe983
ash2686 <ash.centaur@gmail.com>
2022-02-18 09:54:50 +1300

    posts component not working

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 let postSchema = mongoose.Schema({
     name  : String,
     description : String,
-    createdBy : String,
     csvFile : String,
     createdAt : { type:Date , default: new Date() }
 });
